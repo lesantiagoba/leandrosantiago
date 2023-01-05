4b6d0aab431cc066a54f37b98d8dba805cacb5bb
Leandro <lesantiagoba@outlook.com>
2023-01-05 19:40:02 -0400

    Create a skils component Area

---
+++ src/Component/InfoCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './InfoCard.css';
+
+const InfoCard = (Img, Title, DateFrom, DateTo, Info) => {
+    return(
+        <div className="CardContainer">
+            <div className="CardContainer-Logo">
+                <figure>
+                    <img src="https://yt3.ggpht.com/a/AATXAJy559k6O2nQra1TFTCB9ZWSK_L3o1cTI4IXbw=s900-c-k-c0xffffffff-no-rj-mo"/ >
+                </figure>
+            </div>
+            <div className="CardContainer-info">
+                <div className="CardContainer-Title">
+                    <h2>
+                        Seguro Nacional de Salud SeNaSa
+                    </h2>
+                </div>
+                <div className="CardContainer-Date">
+                    <div className="Date_From">
+                        <h2>01/01/2022</h2>
+                    </div>
+                    <div className="Date_To">
+                        <h2>01/01/2022</h2>
+                    </div>
+                </div>
+                <div className="CardContainer-Info">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, explicabo velit? Quam fugiat exercitationem iusto hic dolorem recusandae dolorum ducimus id, numquam minus inventore, distinctio laborum fugit sapiente molestiae asperiores!</p>
+                </div>
+            </div>
+           
+        </div>
+    )
+
+}
+
+export default InfoCard;
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import EducationCard from "../Component/EducationCard";
 import './Experience.css';
+import InfoCard from "../Component/InfoCard";
+
 const Experience = () => {
 
     return(
         <div className="experience-Container">
-            <EducationCard
-               ImgUrl = {"https://www.kcpdynamics.com/wp-content/uploads/Microsoft-Dynamics-AX.png"}
-               nombreInstitucion = {"Seguro Nacional de Salud SeNaSa"}
-               TituloObtenido = {''}
-               Honores = {''}
-               FromDate = {'01/01/2001'}
-               ToDate = {'01/01/2001'}
-               description = {'EducationList[0].description'}
-            />
+            <InfoCard />
+            <InfoCard />
+            <InfoCard />
         </div>
     )
 }
