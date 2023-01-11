471b7bcb65e4088eeb046783a4382553fbda2b43
Leandro <lesantiagoba@outlook.com>
2023-01-10 21:52:06 -0400

    Implement Material UI for create a navbar

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,65 +1,155 @@
-import React from "react";
-import { useState } from "react";
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import './Navbar.css';
-import Experience from "../pages/Experience";
-import menuImage from "../Common/img/Profile.png";
-//const [FormularioContacto, setFormularioContacto] = useState();
 
-const Navbar = (
+const drawerWidth = 240;
+const navItems = [
     {
-         ContactCode = (value) => {
-            return value
-        }
+        id: 1,
+        name: 'Education',
+        Path: '#Education'
+    },
+    {
+        id: 2,
+        name: 'Skills ',
+        Path: '#Skills '
+    },
+    {
+        id: 3,
+        name: 'Portfolio ',
+        Path: '#Portfolio '
+    },
+    {
+        id: 4,
+        name: 'Experience ',
+        Path: '#Experience '
+    },
+    {
+        id: 5,
+        name: 'Contact',
+        Path: '#Contact'
+    },
+];
+
+
+const  NavBar = (
+{
+    props = (item) => {
+        return item;
+    },
+    ContactCode = (value) => {
+        return value
     }
-) => {
-
-    const [navBarshow, setNavBarShow] = useState(100);
-
-    const navClicked = () => {
-       alert("Hola mundo");
-    }
-
-    return(
-            <div className="NavbarContainer">
-            <div className="NavbarContainers-Name">
-            <div onClick={() => navClicked()} className="buton">
-                <img src={menuImage} ></img>
-            </div> 
-            <a className="Links" href="#">Leandro Santiago</a>
-            </div>
-            <div className="NavbarContainers-Links">
-                <nav>
-                    <ul>
-                        <li>
-                            <a  className="Links" href='#Education'>
-                                Education 
-                            </a>
-                        </li>
-                        <li>
-                            <a  className="Links" href='#Skills'>
-                                Skills 
-                            </a>
-                        </li>
-                        <li>
-                            <a  className="Links" href='#Portfolio'>
-                                Portfolio 
-                            </a>
-                        </li>
-                        <li>
-                            <a  className="Links" href='#Experience'>
-                                Experience 
-                            </a>
-                        </li>
-                        <li>
-                            <a onClick={() => ContactCode(0)}  className="Links" href="#Contact">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-        
-    )
 }
 
-export default Navbar;
+
+) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      
+      <AppBar  component="nav" style={{ background: 'black' }}  >
+        <Toolbar >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <a className="Links" href="#">Leandro Santiago</a>
+            
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+                item.id == 5 
+                ? 
+                   <Button key={item.id} sx={{ color: '#fff' }}>
+                    <a onClick={() => ContactCode(0)}  className="Links" href="#Contact">{item.name}</a>
+                  </Button>
+            
+                :
+                <Button key={item.id} sx={{ color: '#fff' }}>
+                    <a  className="Links" href={item.Path}>
+                        {item.name}
+                    </a>
+                </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+NavBar.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default NavBar;
